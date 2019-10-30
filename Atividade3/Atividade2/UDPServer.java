b778cf2b7cbc4e468d1508f447deeaa9dcf47f45
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-10-30 08:37:49 -0300

    Adding exercice 3 (2 of 3)

--- Atividade3/Atividade2/UDPServer.java
+++ Atividade3/Atividade2/UDPServer.java
@@ -1,77 +1,84 @@
+
 /**
  * Code is taken from Computer Networking: A Top-Down Approach Featuring 
  * the Internet, second edition, copyright 1996-2002 J.F Kurose and K.W. Ross, 
  * All Rights Reserved.
  **/
 
-import java.io.*; 
-import java.net.*; 
+import java.io.*;
+import java.net.*;
 import java.time.*;
 
-class UDPServer { 
-    public static void main(String args[]) throws Exception 
-    { 
-	
-	DatagramSocket serverSocket = new DatagramSocket(9876); 
+class UDPServer {
+	public static void main(String args[]) throws Exception {
 
-	byte[] receiveData = new byte[1024]; 
-	byte[] sendData  = new byte[1024]; 
-	
-	while(true) 
-	    { 
-		
-		DatagramPacket receivePacket = 
-		    new DatagramPacket(receiveData, receiveData.length); 
-		serverSocket.receive(receivePacket); 
-		String sentence = new String(receivePacket.getData());
+		DatagramSocket serverSocket = new DatagramSocket(9876);
 
-		InetAddress IPAddress = receivePacket.getAddress(); 
-		
-		int port = receivePacket.getPort(); 
-		
-		String recvOp = sentence.toUpperCase(); 
+		while (true) {
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
+			
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			serverSocket.receive(receivePacket);
+			String sentence = new String(receivePacket.getData());
 
-		System.out.println("Operacao: " + recvOp);
+			InetAddress IPAddress = receivePacket.getAddress();
 
-		String[] operacao = recvOp.split(" ");
+			int port = receivePacket.getPort();
 
-		int OPERACAO_TIPO = 0;
-     		int OPERANDO_1 = 1;
-     		int OPERANDO_2 = 2;
+			String recvOp = sentence.toUpperCase();
 
-		String response = "";
+			System.out.println("#2. Operacao recebida pelo servidor [" + recvOp + "]");
 
-		if(operacao[OPERACAO_TIPO].equals("ADD"))
-			response = String.valueOf(Double.parseDouble(operacao[OPERANDO_1]) + Double.parseDouble(operacao[OPERANDO_2]));
-		else if(operacao[OPERACAO_TIPO].equals("SUB"))
-			response = String.valueOf(Double.parseDouble(operacao[OPERANDO_1]) - Double.parseDouble(operacao[OPERANDO_2]));
-		else if(operacao[OPERACAO_TIPO].equals("MULT"))
-			response = String.valueOf(Double.parseDouble(operacao[OPERANDO_1]) * Double.parseDouble(operacao[OPERANDO_2]));
-		else if(operacao[OPERACAO_TIPO].equals("DIV")){
-			if(Double.parseDouble(operacao[OPERANDO_2]) != 0.0)
-				response = String.valueOf(Double.parseDouble(operacao[OPERANDO_1]) / Double.parseDouble(operacao[OPERANDO_2]));
+			String[] operacao = recvOp.split(" ");
+
+			int OPERACAO_TIPO = 0;
+			int OPERANDO_1 = 1;
+			int OPERANDO_2 = 2;
+
+			String response = "";
+
+			if (operacao[OPERACAO_TIPO].equals("ADD"))
+				response = String
+						.valueOf(Double.parseDouble(operacao[OPERANDO_1]) + Double.parseDouble(operacao[OPERANDO_2]));
+			else if (operacao[OPERACAO_TIPO].equals("SUB"))
+				response = String
+						.valueOf(Double.parseDouble(operacao[OPERANDO_1]) - Double.parseDouble(operacao[OPERANDO_2]));
+			else if (operacao[OPERACAO_TIPO].equals("MULT"))
+				response = String
+						.valueOf(Double.parseDouble(operacao[OPERANDO_1]) * Double.parseDouble(operacao[OPERANDO_2]));
+			else if (operacao[OPERACAO_TIPO].equals("DIV")) {
+				if (Double.parseDouble(operacao[OPERANDO_2]) != 0.0)
+					response = String.valueOf(
+							Double.parseDouble(operacao[OPERANDO_1]) / Double.parseDouble(operacao[OPERANDO_2]));
+				else
+					response = "nao eh possivel dividir";
+			} else if (operacao[OPERACAO_TIPO].equals("EXP"))
+				response = String.valueOf(
+						Math.pow(Double.parseDouble(operacao[OPERANDO_1]), Double.parseDouble(operacao[OPERANDO_2])));
 			else
-				response = "nao eh possivel dividir";
-		}else if(operacao[OPERACAO_TIPO].equals("EXP"))
-			response = String.valueOf(Math.pow(Double.parseDouble(operacao[OPERANDO_1]), Double.parseDouble(operacao[OPERANDO_2])));
-		else
-			response = "erro na sintaxe da operacao";
-		
-		sendData = response.getBytes();
-		
-		DatagramPacket sendPacket = null, ACKPacket = null;
+				response = "erro na sintaxe da operacao";
 
-		while(true){
-			try{
-				sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-				serverSocket.send(sendPacket);
-				ACKPacket = new DatagramPacket(receiveData, receiveData.length);
-				serverSocket.receive(ACKPacket);
-				System.out.println("Confirmando ... " + new String(ACKPacket.getData()));
-			}catch(SocketTimeoutException e){
-                        	System.err.println("Erro ao receber confirmacao do segmento UDP enviado");
-                	}
+			sendData = response.getBytes();
+
+			DatagramPacket sendPacket = null, ACKPacket = null;
+
+			while (true) {
+				try {
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+					serverSocket.send(sendPacket);
+					System.out.println("#3. Enviando resposta da operação [" + response + "]");
+					
+					ACKPacket = new DatagramPacket(receiveData, receiveData.length);
+					serverSocket.receive(ACKPacket);
+					String confirmacao = new String(receiveData);
+					System.out.println("#6. Confirmação que recebeu [" + confirmacao + "]");
+					
+					break;
+				} catch (SocketTimeoutException e) {
+					System.err.println("Erro ao receber confirmacao do segmento UDP enviado");
+				}
+			}
 		}
-	    } 
-    } 
-}  
+	}
+}
